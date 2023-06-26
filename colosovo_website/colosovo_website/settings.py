b6b8ea1ba6ec2a894e1e2774d9997407588291bd
vadim rusu <ruvamd@gmail.com>
2023-06-26 03:32:38 -0700

    .

--- colosovo_website/colosovo_website/settings.py
+++ colosovo_website/colosovo_website/settings.py
@@ -78,6 +78,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+
+        # 'NAME': config('DB_NAME'), 
+        # 'USER': config('DB_USER'),
+        # 'PASSWORD': config('DB_PASSWORD'),
+        # 'HOST': config('DB_HOST'),
+        # 'PORT': config('DB_PORT', cast=int),
     }
 }
 
