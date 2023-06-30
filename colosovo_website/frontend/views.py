a811b32f60638e07661baa74e4e7d0497240158b
vadim rusu <ruvamd@gmail.com>
2023-06-30 00:15:19 -0700

    .

---
+++ colosovo_website/frontend/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def front_page(request):
+    return render(request, 'frontend/front_page.html')
